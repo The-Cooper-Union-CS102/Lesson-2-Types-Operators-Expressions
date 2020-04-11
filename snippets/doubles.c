a2d385b0a8f5ceab4908fbb57957de0f92ae92a5
corynezin <cory.nezin@gmail.com>
2020-04-10 22:02:26 -0400

    add snippets

---
+++ snippets/doubles.c
@@ -0,0 +1,11 @@
+#include <float.h> /* FLT_MAX, FLT_EPSILON, DBL_MAX, DBL_EPSILON, LDBL_MAX
+                      LDBL_EPSILON */
+#include <stdio.h> /* printf */
+
+int main() {
+    double a = -2.7;
+    double b = 5.3;
+    double c = 2e-6;
+    printf("double [max] = %g\n", DBL_MAX);
+    printf("double [smallest] = %g\n", DBL_EPSILON);
+}
---
+++ snippets/floats.c
@@ -0,0 +1,11 @@
+#include <float.h> /* FLT_MAX, FLT_EPSILON, DBL_MAX, DBL_EPSILON, LDBL_MAX
+                      LDBL_EPSILON */
+#include <stdio.h> /* printf */
+
+int main() {
+    float a = -2.7;
+    float b = 5.3;
+    float c = 2e-6;
+    printf("float [max] = %g\n", FLT_MAX);
+    printf("float [smallest] = %g\n", FLT_EPSILON);
+}
