75ed3d01055bcf23f738894fe7bf8e9f150f7d4d
corynezin <cory.nezin@gmail.com>
2020-04-11 15:08:56 -0400

    update precision

--- snippets/division_floats.c
+++ snippets/division_floats.c
@@ -1,10 +1,10 @@
 #include <stdio.h> /* printf */
 
 int main() {
-    float dividend = 31.7;
+    float dividend = 51.7;
     float divisor = 5.3;
     float quotient = dividend / divisor;
     float product = quotient * divisor;
-    printf("%.16f / %.16f = %.16f\n", dividend, divisor, quotient);
-    printf("%.16f = %.16f * %.16f\n", product, quotient, divisor);
+    printf("%.6f / %.6f = %.6f\n", dividend, divisor, quotient);
+    printf("%.6f = %.6f * %.6f\n", product, quotient, divisor);
 }
