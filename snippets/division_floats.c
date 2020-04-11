0e8e8407e40e77c7a8df87376692a1f8fffcc8ad
corynezin <cory.nezin@gmail.com>
2020-04-11 15:06:24 -0400

    update divs

--- snippets/division_floats.c
+++ snippets/division_floats.c
@@ -4,7 +4,7 @@ int main() {
     float dividend = 31.7;
     float divisor = 5.3;
     float quotient = dividend / divisor;
-    float product = quotient * divisor
+    float product = quotient * divisor;
     printf("%.16f / %.16f = %.16f\n", dividend, divisor, quotient);
     printf("%.16f = %.16f * %.16f\n", product, quotient, divisor);
 }
--- snippets/division_ints.c
+++ snippets/division_ints.c
@@ -4,7 +4,7 @@ int main() {
     int dividend = 30;
     int divisor = 4;
     int quotient = dividend / divisor;
-    int remainder = dividend - quotient * divisor;
+    int remaining = dividend - quotient * divisor;
     printf("%d / %d = %d\n", dividend, divisor, quotient);
-    printf("%d = %d * %d + %d\n", dividend, divisor, quotient, remainder);
+    printf("%d = %d * %d + %d\n", dividend, divisor, quotient, remaining);
 }
